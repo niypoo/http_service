ad788099624ebf8789a7b73e47a92fe42708c8a6
Mahmoud Nabhan <niypoo@gmail.com>
2023-09-12 17:34:29 +0300

    first commit

---
+++ lib/http.service.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+
+class HttpService extends GetxService {
+  
+  final GetConnect client = GetConnect();
+
+  Future<HttpService> init({
+    required String baseUrl,
+    required Future<String> Function() getToken,
+  }) async {
+    // BASE URL
+    client.baseUrl = baseUrl;
+
+    // the solution of handshake
+    client.allowAutoSignedCert = true;
+
+    // assign token
+    client.httpClient.addRequestModifier((dynamic request) async {
+      final String token = await getToken();
+      request.headers['Authorization'] = token;
+      return request;
+    });
+
+    //Authenticator will be called 3 times if HttpStatus is
+    client.maxAuthRetries = 3;
+    return this;
+  }
+}
